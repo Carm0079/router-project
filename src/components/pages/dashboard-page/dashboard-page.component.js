eeba2752de026c0dcbbaaf29bfc1281f1c5c84df
Alex Carmichael <carm0079@algonquinlive.com>
2019-02-14 12:35:37 -0500

    adding router implementation

---
+++ src/components/pages/dashboard-page/dashboard-page.component.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'DashboardPage'
+}
+
+// PascalConvention: for 'Class' as in object oriented programming.
+// kebab-convention: for the template and template class (for css).
+// camelCaseConvention: for javascript variables and ids in template.
