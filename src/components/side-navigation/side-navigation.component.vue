09e6cc3c23da99335eb87145e63ba03dd016d1fb
Alex Carmichael <carm0079@algonquinlive.com>
2019-02-14 11:26:56 -0500

    setting up side navigation

---
+++ src/components/side-navigation/side-navigation.component.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="sideNavigation">
+    <el-menu default-active="2" class="side-nav-bar">
+      <el-menu-item index="1">
+        <span>Dashboard</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <span>Dashboard</span>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <span>Dashboard</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script src="./side-navigation.component.js"></script>
+<style src="./side-navigation.component.css" scoped></style>
